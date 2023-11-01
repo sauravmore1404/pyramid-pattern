a14b093af5e48f0d86a90886e6791084e7c82703
Saurav More <sauravmore1404@gmail.com>
2023-11-01 13:35:18 +0530

    updating 3rd pattern

--- 3rd pyramid pattern/script.js
+++ 3rd pyramid pattern/script.js
@@ -5,6 +5,7 @@ const pyaramidPattern=()=>{
       for(let j=0;j<i;j++){string += " * ";}
       for(let j=0;j<2*(num-i);j++){string += " _ ";}
       for(let j=0;j<i;j++){string += " * ";}
+      string += "<br/>"
     }
     return document.getElementById("demo").innerHTML=string
   }
